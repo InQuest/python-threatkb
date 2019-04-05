a3f3664f326dd5b47accf5ccf17d63406c9005c9
rshipp <github@rshipp.com>
2019-04-05 10:44:38 -0500

    Bump version to alpha2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='threatkb',
-    version='1.0.0-alpha1',
+    version='1.0.0-alpha2',
     include_package_data=True,
     py_modules=[
         'threatkb',
